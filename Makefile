264c61dedc920550bae72c613e7d84f4ec9e2718
dgok50 <dgok50@a1mc.ru>
2018-09-12 18:05:21 +0300

    struct add, else

--- Makefile
+++ Makefile
@@ -25,53 +25,16 @@ RM      = rm -f
 
 default: all
 
-all: water_data_write_rtf eltech_dz
+all: game_server
 
-water_data_write_rtf:
-	$(CC) $(CFLAGSL) -o build/water_data_write_rtf water_data_write_rtf.c
-# $(PBCC) $(CFLAGSPB) -o build/water_data_write_rtf.app water_data_write_rtf.c
-	$(W32CC) $(CFLAGSW) -o build/water_data_write_rtf.exe water_data_write_rtf.c
-	$(SU) "$(COPY) $(PWD)/build/water_data_write_rtf /bin/water_data_write_rtf"
-
-tcp_sc: tcp_sc/client.c tcp_sc/server.c
-	$(CC) $(CFLAGSL) tcp_sc/client.c -o build/tcp_sc/client
-	$(PBCC) $(CFLAGSPB) -o build/tcp_sc/client.app tcp_sc/client.c
-	$(W32CC) $(CFLAGSW) -o build/tcp_sc/client.exe tcp_sc/client.c -lws2_32
-	$(CC) $(CFLAGSL) -o build/tcp_sc/server tcp_sc/server.c
-	$(PBCC) $(CFLAGSPB) -o build/tcp_sc/server.app tcp_sc/server.c
-	$(W32CC) $(CFLAGSW) -o build/tcp_sc/server.exe tcp_sc/server.c -lws2_32
-
-eltech_dz:
-	$(CC) $(CFLAGSL) -o build/eltech_dz eltech_dz.c
-	$(PBCC) $(CFLAGSPB) -o build/eltech_dz.app eltech_dz.c
-	$(W32CC) $(CFLAGSW) -o build/eltech_dz.exe eltech_dz.c
-
-#lab2: 
-#	$(CC) $(CFLAGSL) -o build/lab2 l2/main.cpp
-#	$(PBCC) $(CFLAGSPB) -o build/lab2.app l2/main.cpp
-#	$(W32CC) $(CFLAGSW) -o build/lab2.exe l2/main.cpp
-#	
-#lab4: 
-#	$(CC) $(CFLAGSL) -o build/lab4 l4/main.cpp
-#	$(PBCC) $(CFLAGSPB) -o build/lab4.app l4/main.cpp
-#	$(W32CC) $(CFLAGSW) -o build/lab4.exe l4/main.cpp
+game_server:
+	$(CC) $(CFLAGSL) -o build/game_server game_server.c
+# $(PBCC) $(CFLAGSPB) -o build/game_server.app game_server.c
+	$(W32CC) $(CFLAGSW) -o build/game_server.exe game_server.c
 
 clean:
-	$(RM) build/water_data_write_rtf
-	$(RM) build/water_data_write_rtf.app
-	$(RM) build/water_data_write_rtf.exe
-	$(RM) build/eltech_dz
-	$(RM) build/tcp_sc/client
-	$(RM) build/tcp_sc/client.app
-	$(RM) build/tcp_sc/client.exe
-	$(RM) build/tcp_sc/server
-	$(RM) build/tcp_sc/server.app
-	$(RM) build/tcp_sc/server.exe
-#	$(RM) build/lab2
-#	$(RM) build/lab2.app
-#	$(RM) build/lab2.exe
-#	$(RM) build/lab4
-#	$(RM) build/lab4.app
-#	$(RM) build/lab4.exe
+	$(RM) build/game_server
+	$(RM) build/game_server.app
+	$(RM) build/game_server.exe
 
 rebuild: clean all
--- game_server.c
+++ game_server.c
@@ -7,6 +7,9 @@
 #include <string.h>
 #include <stdarg.h>
 
+#define max_players 100
+#define port 228
+
 pthread_mutex_t mutex_login = PTHREAD_MUTEX_INITIALIZER;
 
 pthread_mutex_t mutex_manager = PTHREAD_MUTEX_INITIALIZER;
@@ -15,7 +18,23 @@ pthread_mutex_t mutex_net = PTHREAD_MUTEX_INITIALIZER;
 
 bool manager_st=0, login_st=0, net_st=0;
 
-int stop = 0;
+struct player_s {
+    unsigned long int_id = 0;
+    char ip1, ip2, ip3, ip4;
+    char name[50];
+    int sock;
+    long x, y, z;
+    Bool st = 0;
+    char i_status = 0
+};
+
+struct player_s player[max_players];
+//int sock[max_players];
+
+bool sock_stat[max_players];
+
+
+int stop = 0; 
 
 //Функция завершения работы
 int stop_all(void) {
@@ -26,11 +45,11 @@ int stop_all(void) {
         ptolog( debug,  LOG_F, "\nЗавершение работы.");
         for(;;)
         {
-            if(rrd_trs == 1 || sens_trs == 1 || count_trs == 1 || dev_trs == 1)
+            if(manager_st == 0 || login_st == 0 || net_st == 0)
             {
                 ptolog( debug,  LOG_F, "\n");
                 sleep(3);
-                if(debug == 1)ptolog( debug,  LOG_F, "\nВсе потоки остановлены.\n");
+                if(debug == 1) ptolog( debug,  LOG_F, "\nВсе потоки остановлены.\n");
                 remove("term.pid");
                 stop=2;
                 break;
@@ -41,6 +60,9 @@ int stop_all(void) {
 
 void game_server()
 {
+    explicit_bzero(sock_stat, max_players);
+    bzero(sock, max_players);
+
     pthread_attr_t attr;
     pthread_t info;
     pthread_attr_init(&attr);
@@ -77,8 +99,38 @@ void* login(void *arg){
 
 void* main_net(void *arg){
 	net_st = 1;
+	int listener, free_id = 0;
+	char buf[1024];
+	
+	struct sockaddr_in addr;
+	
+	listener = socket(AF_INET, SOCK_STREAM, 0);
+	
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(port);
+	addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	
+	if (bind(listener, (struct sockaddr *)&addr, sizeof(addr)) <0 )
+	{
+	    perror("BIND!!");
+	    exit(2);
+	}
+
+
 	while(stop == 0)
 	{
+		free_id = -1;
+		for(int id; id < max_players; id++)
+		{
+		    if(sock_stat[id] == 0)
+			free_id = id;
+		}
+		if(free_id != -1){
+		    sock[free_id]=accept(listener, NULL, NULL);
+		    sock_stat[id] = 1;
+		    send(sock[free_id], "001, CONNECT ACCEPTED", 21, 0);
+		    
+		}
 		
 	}
 	net_st = 0;	
